4e0f566b3a666dd1d28a71235f748f44815a85fb
Samuel Braun <office.samigo.a@gmail.com>
2020-09-10 18:28:08 +0200

    Created Header

---
+++ js/debug.js
@@ -0,0 +1,10 @@
+
+// Check if local version
+if (window.location.href.includes("Blog")) {
+
+    // Make reload easier
+    window.addEventListener("focus", () => {
+        document.location = document.location;
+    });
+
+}
