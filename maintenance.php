f0ddf24bc11ba16f755ac9ad8f4adb136be90811
Samuel Braun <42311986+MindLaborDev@users.noreply.github.com>
2020-10-14 15:49:25 +0200

    robots

--- maintenance.php
+++ maintenance.php
@@ -4,6 +4,7 @@
 		<title>MindLabor - Page not found</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="robots" content="noindex, follow">
 
 		<link rel="canonical" href="https://www.mindlabor.dev/">
 		<link rel="shortcut icon" href="assets/icons/ico/favicon-32x32.png" type="image/x-icon">
